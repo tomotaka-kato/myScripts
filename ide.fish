0ad1ce9be2f8f7ded689d3306122bb344a9f1585
SF Tomo <tomo@SFs-MacBook-Pro.local>
2019-11-22 10:58:55 +0900

    ideコマンド変更

--- ide.fish
+++ ide.fish
@@ -2,4 +2,7 @@ function ide
   tmux split-window -v -p 30
   tmux split-window -h -p 66
   tmux split-window -h -p 50
+  tmux select-pane -t 0
+  tmux split-window -h -p 25
+  tmux select-pane -t 0
 end
