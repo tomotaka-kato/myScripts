c21257432e87bdb3ab56db070ea83b6b996be69f
SF Tomo <tomo@SFs-MacBook-Pro.local>
2019-11-28 18:17:12 +0900

    scalaコマンド追加

--- ide.fish
+++ ide.fish
@@ -1,8 +1,27 @@
 function ide
+  set length (count $argv)
+  if test $length -ne 1
+    _common
+    return
+  end
+
+  switch $argv
+    case scala
+      _scala
+  case '*'
+    echo "$argv is not defined"
+  end
+end
+
+function _common
   tmux split-window -v -p 30
   tmux split-window -h -p 66
   tmux split-window -h -p 50
   tmux select-pane -t 0
   tmux split-window -h -p 25
   tmux select-pane -t 0
+end
+
+function _scala
+  tmux split-window -v -p 30
 end
